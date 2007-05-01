2438f4e741c32ff7a8ea8956d7955d50a765f796
Tim Pope <code@tpope.net>
2007-05-01 19:11:52 +0000

    Extracted syntaxes to a separate file

---
+++ lib/ldaptor/syntaxes.rb
@@ -0,0 +1,117 @@
+module Ldaptor
+
+  # RFC 2252.  Second column is "Human Readable"
+  SYNTAX_STRING = <<-EOF unless defined? SYNTAX_STRING
+ACI Item                        N  1.3.6.1.4.1.1466.115.121.1.1
+Access Point                    Y  1.3.6.1.4.1.1466.115.121.1.2
+Attribute Type Description      Y  1.3.6.1.4.1.1466.115.121.1.3
+Audio                           N  1.3.6.1.4.1.1466.115.121.1.4
+Binary                          N  1.3.6.1.4.1.1466.115.121.1.5
+Bit String                      Y  1.3.6.1.4.1.1466.115.121.1.6
+Boolean                         Y  1.3.6.1.4.1.1466.115.121.1.7
+Certificate                     N  1.3.6.1.4.1.1466.115.121.1.8
+Certificate List                N  1.3.6.1.4.1.1466.115.121.1.9
+Certificate Pair                N  1.3.6.1.4.1.1466.115.121.1.10
+Country String                  Y  1.3.6.1.4.1.1466.115.121.1.11
+DN                              Y  1.3.6.1.4.1.1466.115.121.1.12
+Data Quality Syntax             Y  1.3.6.1.4.1.1466.115.121.1.13
+Delivery Method                 Y  1.3.6.1.4.1.1466.115.121.1.14
+Directory String                Y  1.3.6.1.4.1.1466.115.121.1.15
+DIT Content Rule Description    Y  1.3.6.1.4.1.1466.115.121.1.16
+DIT Structure Rule Description  Y  1.3.6.1.4.1.1466.115.121.1.17
+DL Submit Permission            Y  1.3.6.1.4.1.1466.115.121.1.18
+DSA Quality Syntax              Y  1.3.6.1.4.1.1466.115.121.1.19
+DSE Type                        Y  1.3.6.1.4.1.1466.115.121.1.20
+Enhanced Guide                  Y  1.3.6.1.4.1.1466.115.121.1.21
+Facsimile Telephone Number      Y  1.3.6.1.4.1.1466.115.121.1.22
+Fax                             N  1.3.6.1.4.1.1466.115.121.1.23
+Generalized Time                Y  1.3.6.1.4.1.1466.115.121.1.24
+Guide                           Y  1.3.6.1.4.1.1466.115.121.1.25
+IA5 String                      Y  1.3.6.1.4.1.1466.115.121.1.26
+INTEGER                         Y  1.3.6.1.4.1.1466.115.121.1.27
+JPEG                            N  1.3.6.1.4.1.1466.115.121.1.28
+LDAP Syntax Description         Y  1.3.6.1.4.1.1466.115.121.1.54
+LDAP Schema Definition          Y  1.3.6.1.4.1.1466.115.121.1.56
+LDAP Schema Description         Y  1.3.6.1.4.1.1466.115.121.1.57
+Master And Shadow Access Points Y  1.3.6.1.4.1.1466.115.121.1.29
+Matching Rule Description       Y  1.3.6.1.4.1.1466.115.121.1.30
+Matching Rule Use Description   Y  1.3.6.1.4.1.1466.115.121.1.31
+Mail Preference                 Y  1.3.6.1.4.1.1466.115.121.1.32
+MHS OR Address                  Y  1.3.6.1.4.1.1466.115.121.1.33
+Modify Rights                   Y  1.3.6.1.4.1.1466.115.121.1.55
+Name And Optional UID           Y  1.3.6.1.4.1.1466.115.121.1.34
+Name Form Description           Y  1.3.6.1.4.1.1466.115.121.1.35
+Numeric String                  Y  1.3.6.1.4.1.1466.115.121.1.36
+Object Class Description        Y  1.3.6.1.4.1.1466.115.121.1.37
+Octet String                    Y  1.3.6.1.4.1.1466.115.121.1.40
+OID                             Y  1.3.6.1.4.1.1466.115.121.1.38
+Other Mailbox                   Y  1.3.6.1.4.1.1466.115.121.1.39
+Postal Address                  Y  1.3.6.1.4.1.1466.115.121.1.41
+Protocol Information            Y  1.3.6.1.4.1.1466.115.121.1.42
+Presentation Address            Y  1.3.6.1.4.1.1466.115.121.1.43
+Printable String                Y  1.3.6.1.4.1.1466.115.121.1.44
+Substring Assertion             Y  1.3.6.1.4.1.1466.115.121.1.58
+Subtree Specification           Y  1.3.6.1.4.1.1466.115.121.1.45
+Supplier Information            Y  1.3.6.1.4.1.1466.115.121.1.46
+Supplier Or Consumer            Y  1.3.6.1.4.1.1466.115.121.1.47
+Supplier And Consumer           Y  1.3.6.1.4.1.1466.115.121.1.48
+Supported Algorithm             N  1.3.6.1.4.1.1466.115.121.1.49
+Telephone Number                Y  1.3.6.1.4.1.1466.115.121.1.50
+Teletex Terminal Identifier     Y  1.3.6.1.4.1.1466.115.121.1.51
+Telex Number                    Y  1.3.6.1.4.1.1466.115.121.1.52
+UTC Time                        Y  1.3.6.1.4.1.1466.115.121.1.53
+EOF
+
+  SYNTAXES = {} unless defined? SYNTAXES
+  SYNTAX_STRING.each_line do |line|
+    a, b = line.chomp.split(/ [YN]  /)
+    a.rstrip!
+    SYNTAXES[b] = a
+  end
+
+  module Syntaxes
+
+    module DirectoryString
+      def self.parse(string)
+        string
+      end
+      def self.format(string)
+        string.to_str
+      end
+    end
+
+    module Boolean
+      def self.parse(string)
+        return string == "TRUE"
+      end
+      def self.format(boolean)
+        case boolean
+        when "TRUE",  true  then "TRUE"
+        when "FALSE", false then "FALSE"
+        else raise TypeError, "boolean expected", caller
+        end
+      end
+    end
+
+    module INTEGER
+      def self.parse(string)
+        return string.to_i
+      end
+      def self.format(integer)
+        Integer(integer).to_s
+      end
+    end
+
+    module GeneralizedTime
+      def self.parse(string)
+        require 'time'
+        Time.parse(string)
+      end
+      def self.format(time)
+        time.utc.strftime("%Y%m%d%H%M%S")+".%06dZ" % (time.usec/100_000)
+      end
+    end
+
+  end
+
+end
