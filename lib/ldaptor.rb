f1df0aa2b68e8ee78c26cebf2cf2ded825fdd38d
Tim Pope <code@tpope.net>
2007-05-08 19:34:14 +0000

    Prepare for refactoring into two separate base classes

--- lib/ldaptor.rb
+++ lib/ldaptor.rb
@@ -24,6 +24,19 @@ module Ldaptor
   class RecordNotFound < Error
   end
 
+  def self.clone_ldap_hash(attributes)
+    hash = Hash.new {|h,k| h[k] = [] }
+    attributes.each do |k,v|
+      k = k.kind_of?(Symbol) ?  k.to_s.gsub('_','-') : k.dup
+      if v.kind_of?(LDAP::DN)
+        hash[k] = [v.dup]
+      else
+        hash[k] = Array(v).map {|x| x.dup rescue x}
+      end
+    end
+    hash
+  end
+
   def self.build_hierarchy(connection,base_dn = nil,&block)
     klass = Class.new(Base)
     klass.connection = connection
@@ -37,7 +50,6 @@ module Ldaptor
     klass = Class.new(Base)
     klass.connection = connection
     klass.base_dn = base_dn
-    klass.instance_variable_set(:@name, false)
     klass.instance_variable_set(:@oid,  false)
     klass
   end
@@ -46,9 +58,9 @@ module Ldaptor
 
     def initialize(data = {})
       raise TypeError, "abstract class initialized", caller if self.class.oid.nil? || self.class.abstract?
-      @attributes = self.class.clone_ldap_hash({'objectClass' => self.class.object_classes}.merge(data))
-      if dn = @attributes.delete('dn') && !dn.empty?
-        @dn = LDAP::DN(dn.first,self)
+      @attributes = Ldaptor.clone_ldap_hash({'objectClass' => self.class.object_classes}.merge(data))
+      if dn = @attributes.delete('dn')
+        @dn = LDAP::DN(dn.first,self) if dn.first
       end
     end
 
@@ -78,6 +90,9 @@ module Ldaptor
       end
     end
 
+    def namespace
+      self.class.namespace
+    end
     def connection
       self.class.connection
     end
@@ -178,13 +193,16 @@ module Ldaptor
     end
 
     attr_reader :attributes
+    def attribute_names
+      attributes.keys
+    end
 
     def ldap_ancestors
-      self.class.ldap_ancestors | objectClass.map {|c| self.class.root.const_get(c.ldapitalize(true))}
+      self.class.ldap_ancestors | objectClass.map {|c| self.namespace.const_get(c.ldapitalize(true))}
     end
 
     def aux
-      objectClass.map {|c| self.class.root.const_get(c.ldapitalize(true))} - self.class.ldap_ancestors
+      objectClass.map {|c| self.namespace.const_get(c.ldapitalize(true))} - self.class.ldap_ancestors
     end
 
     def must(all = true)
@@ -251,7 +269,7 @@ module Ldaptor
     end
 
     def search(options)
-      self.class.root.search({:base => dn}.merge(options))
+      self.namespace.search({:base => dn}.merge(options))
     end
 
     def save
@@ -264,7 +282,7 @@ module Ldaptor
         connection.add(dn, @attributes)
       end
       @original_attributes = @attributes
-      @attributes = self.class.clone_ldap_hash(@original_attributes)
+      @attributes = Ldaptor.clone_ldap_hash(@original_attributes)
       self
     end
 
@@ -287,20 +305,151 @@ module Ldaptor
 
     class << self
 
-      def clone_ldap_hash(attributes)
-        hash = Hash.new {|h,k| h[k] = [] }
-        attributes.each do |k,v|
-          hash[k.kind_of?(Symbol) ?  k.to_s.gsub('_','-') : k.dup] = Array(v).map {|x| x.dup rescue x}
+      def instantiate(attributes, namespace = nil) # ObjectClass
+        if klass = @subclasses.to_a.find {|c| attributes["objectClass"].to_a.include?(c.object_class)}
+          return klass.send(:instantiate,attributes)
+        elsif klass = self.namespace.const_get(attributes["objectClass"].last.to_s.ldapitalize(true)) rescue nil
+          if klass != self
+            return klass.send(:instantiate,attributes)
+          end
         end
-        hash
+        obj = allocate
+        obj.instance_variable_set(:@dn,Array(attributes.delete('dn')).first)
+        obj.instance_variable_set(:@original_attributes,attributes)
+        obj.instance_variable_set(:@attributes,Ldaptor.clone_ldap_hash(attributes))
+        obj.instance_variable_set(:@namespace, namespace || @namespace)
+        obj
       end
+      protected :instantiate
 
-      def has_attribute?(attribute)
+      def has_attribute?(attribute) # ObjectClass
         attribute = LDAP.escape(attribute)
         may.include?(attribute) || must.include?(attribute)
       end
 
-      def self.inheritable_accessor(*names)
+      def create_accessors # ObjectClass
+        (may(false) + must(false)).each do |attr|
+          method = attr.to_s.tr_s('-_','_-')
+          define_method("#{method}") { read_attribute(attr) }
+          unless attribute_types[attr].no_user_modification?
+            define_method("#{method}="){ |value| write_attribute(attr,value) }
+          end
+        end
+      end
+
+      def ldap_ancestors # ObjectClass
+        ancestors.select {|o| o.respond_to?(:oid) && o.oid }
+      end
+
+      def root # ObjectClass
+        @namespace || ancestors.detect {|o| o.respond_to?(:oid) && o.oid.nil? }
+      end
+
+      def namespace
+        root
+      end
+
+      def may(all = true) # ObjectClass
+        if all
+          nott = []
+          ldap_ancestors.inject([]) do |memo,klass|
+            memo |= Array(klass.may(false))
+            if dit = klass.dit_content_rule
+              memo |= Array(dit.may)
+              nott |= Array(dit.not)
+              # Array(dit.aux).each do |aux|
+                # memo |= self.namespace.const_get(aux.ldapitalize(true)).may(false)
+              # end
+            end
+            memo - nott
+          end
+        else
+          Array(@may)
+        end
+      end
+
+      def must(all = true) # ObjectClass
+        if all
+          ldap_ancestors.inject([]) do |memo,klass|
+            memo |= Array(klass.must(false))
+            if dit = klass.dit_content_rule
+              memo |= Array(dit.must)
+              # Array(dit.aux).each do |aux|
+                # memo |= self.namespace.const_get(aux.ldapitalize(true)).must(false)
+              # end
+            end
+            memo
+          end.flatten.uniq
+        else
+          Array(@must)
+        end
+      end
+
+      def aux # ObjectClass
+        if dit_content_rule
+          Array(dit_content_rule.aux)
+        else
+          []
+        end
+      end
+
+      def attributes(all = true) # ObjectClass
+        may(all) + must(all)
+      end
+
+      def object_class # ObjectClass
+        @object_class || names.first
+      end
+
+      def object_classes # ObjectClass
+        ldap_ancestors.map {|a| a.object_class}.compact.reverse.uniq
+      end
+
+      alias objectClass object_classes # ObjectClass
+
+      def inherited(subclass)
+        # ObjectClass
+        @subclasses ||= []
+        @subclasses << subclass
+        # Namespace
+        if oid == false
+          subclass.send(:build_hierarchy)
+        end
+        super
+      end
+
+      def build_hierarchy # ?????
+        raise TypeError, "cannot build hierarchy for a named class", caller if oid
+        klasses = raw_schema("objectClasses").to_a.map do |k|
+          Ldaptor::Schema::ObjectClass.new(k)
+        end.compact
+        add_constants(self,klasses,self)
+        self.base_dn ||= server_default_base_dn
+        nil
+      end
+
+      def add_constants(mod,klasses,superclass) # ?????
+        klasses.each do |sub|
+          if superclass.names.include?(sub.sup) || superclass.names.empty? && sub.sup == nil
+            klass = Class.new(superclass)
+            %w(oid name desc sup must may).each do |prop|
+              klass.instance_variable_set("@#{prop}", sub.send(prop))
+            end
+            %w(obsolete abstract structural auxiliary).each do |prop|
+              klass.instance_variable_set("@#{prop}", sub.send("#{prop}?"))
+            end
+            klass.instance_variable_set(:@namespace, mod)
+            Array(sub.name).each do |name|
+              mod.const_set(name.ldapitalize(true), klass)
+            end
+            klass.send(:create_accessors)
+            add_constants(self, klasses, klass)
+          end
+        end
+      end
+      private :add_constants
+
+      def self.inheritable_accessor(*names) # Namespace
         names.each do |name|
           define_method name do
             val = instance_variable_get("@#{name}")
@@ -314,10 +463,11 @@ module Ldaptor
       end
 
       inheritable_accessor :connection, :base_dn
-      def base_dn=(dn)
+      def base_dn=(dn) # Namespace
         @base_dn = LDAP::DN(dn,self)
       end
 
+      # Namespace
       attr_reader :oid, :desc, :sup
       %w(obsolete abstract structural auxiliary).each do |attr|
         class_eval("def #{attr}?; !! @#{attr}; end")
@@ -331,69 +481,7 @@ module Ldaptor
         # names.first
       # end
 
-      def create_accessors
-        (may(false) + must(false)).each do |attr|
-          method = attr.to_s.tr_s('-_','_-')
-          define_method("#{method}") { read_attribute(attr) }
-          unless attribute_types[attr].no_user_modification?
-            define_method("#{method}="){ |value| write_attribute(attr,value) }
-          end
-        end
-      end
-
-      def ldap_ancestors
-        ancestors.select {|o| o.respond_to?(:oid) && o.oid }
-      end
-
-      def root
-        ancestors.detect {|o| o.respond_to?(:oid) && o.oid.nil? }
-      end
-
-      def may(all = true)
-        if all
-          nott = []
-          ldap_ancestors.inject([]) do |memo,klass|
-            memo |= Array(klass.may(false))
-            if dit = klass.dit_content_rule
-              memo |= Array(dit.may)
-              nott |= Array(dit.not)
-              # Array(dit.aux).each do |aux|
-                # memo |= root.const_get(aux.ldapitalize(true)).may(false)
-              # end
-            end
-            memo - nott
-          end
-        else
-          Array(@may)
-        end
-      end
-
-      def must(all = true)
-        if all
-          ldap_ancestors.inject([]) do |memo,klass|
-            memo |= Array(klass.must(false))
-            if dit = klass.dit_content_rule
-              memo |= Array(dit.must)
-              # Array(dit.aux).each do |aux|
-                # memo |= root.const_get(aux.ldapitalize(true)).must(false)
-              # end
-            end
-            memo
-          end.flatten.uniq
-        else
-          Array(@must)
-        end
-      end
-
-      def aux
-        if dit_content_rule
-          Array(dit_content_rule.aux)
-        else
-          []
-        end
-      end
-
-      def root_dse(attrs = nil)
+      def root_dse(attrs = nil) # Namespace
         attrs ||= %w[
           objectClass
           subschemaSubentry
@@ -416,7 +504,7 @@ module Ldaptor
         )
       end
 
-      def raw_schema(attrs = nil)
+      def raw_schema(attrs = nil) # Namespace
         attrs ||= %w[
           objectClass
           objectClasses
@@ -438,16 +526,12 @@ module Ldaptor
         )
       end
 
-      def schema
-        instantiate(raw_schema)
+      def schema # Namespace
+        instantiate(raw_schema,self.namespace)
       end
 
-      def attributes(all = true)
-        may(all) + must(all)
-      end
-
-      def attribute_types
-        return root.attribute_types unless self == root
+      def attribute_types # Namespace
+        return self.namespace.attribute_types unless self == self.namespace
         @attribute_types ||= raw_schema("attributeTypes").inject({}) do |hash,val|
           at = Ldaptor::Schema::AttributeType.new(val)
           hash[at.oid] = at
@@ -459,8 +543,8 @@ module Ldaptor
         @attribute_types
       end
 
-      def dit_content_rules
-        return root.dit_content_rules unless self == root
+      def dit_content_rules # Namespace
+        return self.namespace.dit_content_rules unless self == self.namespace
         @dit_content_rules ||= raw_schema("dITContentRules").inject({}) do |hash,val|
           dit = Ldaptor::Schema::DITContentRule.new(val)
           hash[dit.oid] = dit
@@ -471,40 +555,11 @@ module Ldaptor
         end
       end
 
-      def dit_content_rule
+      def dit_content_rule # Namespace
         dit_content_rules[oid]
       end
 
-      def object_class
-        @object_class || names.first
-      end
-
-      def object_classes
-        ldap_ancestors.map {|a| a.object_class}.compact.reverse.uniq
-      end
-
-      alias objectClass object_classes
-
-      def inherited(subclass)
-        @subclasses ||= []
-        @subclasses << subclass
-        if oid == false
-          subclass.send(:build_hierarchy)
-        end
-        super
-      end
-
-      def build_hierarchy
-        raise TypeError, "cannot build hierarchy for a named class", caller if oid
-        klasses = raw_schema("objectClasses").to_a.map do |k|
-          Ldaptor::Schema::ObjectClass.new(k)
-        end.compact
-        add_constants(self,klasses,self)
-        self.base_dn ||= server_default_base_dn
-        nil
-      end
-
-      def server_default_base_dn
+      def server_default_base_dn # Namespace
         result = root_dse(%w(defaultNamingContext namingContexts))
         if result
           result["defaultNamingContext"].to_a.first ||
@@ -512,79 +567,27 @@ module Ldaptor
         end
       end
 
-      def add_constants(mod,klasses,superclass)
-        klasses.each do |sub|
-          if superclass.names.include?(sub.sup) || superclass.names.empty? && sub.sup == nil
-            klass = Class.new(superclass)
-            %w(oid name desc sup must may).each do |prop|
-              klass.instance_variable_set("@#{prop}", sub.send(prop))
-            end
-            %w(obsolete abstract structural auxiliary).each do |prop|
-              klass.instance_variable_set("@#{prop}", sub.send("#{prop}?"))
-            end
-            klass.instance_variable_set(:@module, mod)
-            Array(sub.name).each do |name|
-              mod.const_set(name.ldapitalize(true), klass)
-            end
-            klass.send(:create_accessors)
-            add_constants(mod, klasses, klass)
-          end
-        end
-      end
-      private :add_constants
-
-      def instantiate(attributes)
-        subclasses = @subclasses || []
-        if klass = subclasses.find {|c| attributes["objectClass"].to_a.include?(c.object_class)}
-          return klass.send(:instantiate,attributes)
-        elsif klass = root.const_get(attributes["objectClass"].last.to_s.ldapitalize(true)) rescue nil
-          if klass != self
-            return klass.send(:instantiate,attributes)
-          end
-        end
-        obj = allocate
-        obj.instance_variable_set(:@dn,Array(attributes.delete('dn')).first)
-        obj.instance_variable_set(:@original_attributes,attributes)
-        obj.instance_variable_set(:@attributes,clone_ldap_hash(attributes))
-        obj
-      end
-      protected :instantiate
-
-      def /(*args)
+      def /(*args) # Namespace
         find(base_dn.send(:/,*args))
       end
 
-      def [](*args)
-        find(base_dn[*args])
-      end
-
-      def *(filter)
-        search(:filter => filter, :scope => :onelevel)
-      end
-      def **(filter)
-        search(:filter => filter, :scope => :subtree)
-      end
-
-      def children(type = nil, name = nil)
-        if name && name != :*
-          self/{type => name}
-        elsif type
-          search(:filter => {type => :*}, :scope => :onelevel)
+      def [](*args) # Namespace
+        if args.empty?
+          find(base_dn)
         else
-          search(:scope => :onelevel)
+          find(base_dn[*args])
         end
       end
 
-      # def method_missing(method,*args,&block)
-        # if args.size == 1
-          # children(method.to_s.ldapitalize,*args)
-        # else
-          # super
-        # end
-      # end
+      def *(filter) # Namespace
+        search(:filter => filter, :scope => :onelevel)
+      end
+      def **(filter) # Namespace
+        search(:filter => filter, :scope => :subtree)
+      end
 
       private
-      def paged_results_control(cookie = "", size = 126)
+      def paged_results_control(cookie = "", size = 126) # Namespace
         # values above 126 cause problems for slapd, as determined by net/ldap
         ::LDAP::Control.new(
           # ::LDAP::LDAP_CONTROL_PAGEDRESULTS,
@@ -594,7 +597,7 @@ module Ldaptor
         )
       end
 
-      def search_options(options = {})
+      def search_options(options = {}) # Namespace
         options = options.dup
         options[:instantiate] = true unless options.has_key?(:instantiate)
         options.delete(:attributes_only) if options[:instantiate]
@@ -613,7 +616,7 @@ module Ldaptor
         options
       end
 
-      def search_parameters(options = {})
+      def search_parameters(options = {}) # Namespace
         case options[:sort]
         when Proc, Method then s_attr, s_proc = nil, options[:sort]
         else s_attr, s_proc = options[:sort], nil
@@ -631,7 +634,7 @@ module Ldaptor
         ]
       end
 
-      def raw_ldap_search(options = {}, &block)
+      def raw_ldap_search(options = {}, &block) # Namespace
         cookie = ""
         options = search_options(options)
         parameters = search_parameters(options)
@@ -643,12 +646,11 @@ module Ldaptor
           cookie = ctrl && ctrl.decode.last
           cookie = nil if cookie.to_s.empty?
         end
-        ary
       ensure
         connection.set_option(LDAP::LDAP_OPT_SERVER_CONTROLS,[]) rescue nil
       end
 
-      def raw_net_ldap_search(options = {}, &block)
+      def raw_net_ldap_search(options = {}, &block) # Namespace
         connection.search(options.merge(:return_result => false)) do |entry|
           hash = {}
           entry.each do |attr,val|
@@ -668,7 +670,7 @@ module Ldaptor
         end
       end
 
-      def raw_adapter_search(options = {}, &block)
+      def raw_adapter_search(options = {}, &block) # Namespace
         if defined?(::LDAP::Conn) && connection.kind_of?(::LDAP::Conn)
           raw_ldap_search(options,&block)
         elsif defined?(::Net::LDAP) && connection.kind_of?(::Net::LDAP)
@@ -678,7 +680,7 @@ module Ldaptor
         end
       end
 
-      def find_one(dn,options)
+      def find_one(dn,options) # Namespace
         objects = search(options.merge(:base => dn, :scope => :base))
         unless objects.size == 1
           raise RecordNotFound, "record not found for #{dn}", caller
@@ -709,7 +711,7 @@ module Ldaptor
           one_attribute = nil
         end
         raw_adapter_search(options) do |entry|
-          entry = instantiate(entry) if options[:instantiate]
+          entry = instantiate(entry,self.namespace) if options[:instantiate]
           entry = entry[one_attribute] if one_attribute
           ary << entry
           block.call(entry) if block_given?
