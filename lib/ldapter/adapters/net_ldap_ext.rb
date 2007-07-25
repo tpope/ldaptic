a71fdcb33555ad53c674ddd9a810b7353c33c865
Tim Pope <code@tpope.net>
2007-07-25 17:15:17 +0000

    Fix scope issues in new rename method

--- lib/ldapter/adapters/net_ldap_ext.rb
+++ lib/ldapter/adapters/net_ldap_ext.rb
@@ -1,11 +1,13 @@
 require 'net/ldap'
-# Monkey-patched in support for new superior.
 class Net::LDAP # :nodoc:
+
+  # Erroneously descends from Exception
   remove_const(:LdapError)
   class LdapError < RuntimeError # :nodoc:
   end
 
   class Connection # :nodoc:
+    # Monkey-patched in support for new superior.
     def rename args
       old_dn = args[:olddn] or raise "Unable to rename empty DN"
       new_rdn = args[:newrdn] or raise "Unable to rename to empty RDN"
@@ -18,7 +20,7 @@ class Net::LDAP # :nodoc:
       pkt = [next_msgid.to_ber, request].to_ber_sequence
       @conn.write pkt
 
-      (be = @conn.read_ber(AsnSyntax)) && (pdu = LdapPdu.new( be )) && (pdu.app_tag == 13) or raise LdapError.new( "response missing or invalid" )
+      (be = @conn.read_ber(AsnSyntax)) && (pdu = Net::LdapPdu.new( be )) && (pdu.app_tag == 13) or raise LdapError.new( "response missing or invalid" )
       pdu.result_code
     end
   end
