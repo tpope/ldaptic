7e689c94eca404d57195557dd0bbf3391396a76b
Tim Pope <code@tpope.net>
2011-01-21 19:11:59 -0500

    Fix test name

--- test/ldapter_entry_test.rb
+++ test/ldapter_entry_test.rb
@@ -1,7 +1,7 @@
 require File.join(File.dirname(File.expand_path(__FILE__)),'test_helper')
 require 'ldapter'
 
-class LdapterActiveModelTest < Test::Unit::TestCase
+class LdapterEntryTest < Test::Unit::TestCase
 
   def test_human_attribute_name
     assert_equal 'Given name', Ldapter::Entry.human_attribute_name(:givenName)
